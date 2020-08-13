4ed368fa9a0d82ba40046d1ba986441e8087b93c
Amit Manerkar (CW) <Amit.Manerkar@jda.com>
2020-08-13 21:54:11 +0530

    Skipped Failing Test

--- cypress/tests/uitests/tmLogin.spec.js
+++ cypress/tests/uitests/tmLogin.spec.js
@@ -3,7 +3,7 @@ describe('Transport Manager Tests', () => {
     cy.visit('http://md1npdvtms20.dev.corp.local:7001/tm/framework/Frame.jsp')
   })
 
-  xit('Login Test', () => {
+  it.skip('Login Test', () => {
     expect(true).to.equal(true)
     cy.login('AM', 'AM')
   })
